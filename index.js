1f3fb4544cd27e2c4f7ff8276c60ca597040290f
Kellye Greene <kellyecodes@gmail.com>
2018-12-21 15:03:51 -0500

    updates lab

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+const animal = 'dog'
 
 function myAnimal() {
   return animal
@@ -8,7 +8,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal = 'cat'
+  let animal = 'cat'
 
   return animal
 }
