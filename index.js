4e4a24a86e0085e44dabb9c38257e59cf78b8db4
jenmyers <jen.myers@flatironschool.com>
2018-12-26 11:59:40 -0600

    Add updated test files

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const testVar = {}
+animal = ""
 
-function testFunc() {
-  return "hi"
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
+}
+
+function add2(n) {
+  return n + two
+
+  // Feel free to move things around!
+  two = 2
 }
